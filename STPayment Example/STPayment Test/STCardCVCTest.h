c15d717678df02067b96463cb1bf714f8ab06587
Alex MacCaw <alex@stripe.com>
2013-02-06 11:22:20 -0800

    add tests

---
+++ STPayment Example/STPayment Test/STCardCVCTest.h
@@ -0,0 +1,13 @@
+//
+//  STCardCVCTest.h
+//  STPayment Example
+//
+//  Created by Alex MacCaw on 2/6/13.
+//  Copyright (c) 2013 Stripe. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface STCardCVCTest : SenTestCase
+
+@end
---
+++ STPayment Example/STPayment Test/STPaymentTest.h
@@ -0,0 +1,13 @@
+//
+//  STPayment_Test.h
+//  STPayment Test
+//
+//  Created by Alex MacCaw on 2/6/13.
+//  Copyright (c) 2013 Stripe. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface STPaymentTest : SenTestCase
+
+@end
