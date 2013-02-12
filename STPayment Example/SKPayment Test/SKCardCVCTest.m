80b2b639e3c01a1972bd7430091cc21fd17079ea
Alex MacCaw <alex@stripe.com>
2013-02-12 15:52:54 -0800

    complete rename

--- STPayment Example/SKPayment Test/SKCardCVCTest.m
+++ STPayment Example/SKPayment Test/SKCardCVCTest.m
@@ -1,6 +1,6 @@
 //
 //  SKCardCVCTest.m
-//  STPayment Example
+//  SKPayment Example
 //
 //  Created by Alex MacCaw on 2/6/13.
 //  Copyright (c) 2013 Stripe. All rights reserved.
--- STPayment Example/SKPayment Test/SKPaymentTest.m
+++ STPayment Example/SKPayment Test/SKPaymentTest.m
@@ -1,6 +1,6 @@
 //
-//  STPayment_Test.m
-//  STPayment Test
+//  SKPayment_Test.m
+//  SKPayment Test
 //
 //  Created by Alex MacCaw on 2/6/13.
 //  Copyright (c) 2013 Stripe. All rights reserved.
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    //STFail(@"Unit tests are not implemented yet in STPayment Test");
+    //STFail(@"Unit tests are not implemented yet in SKPayment Test");
 }
 
 @end
