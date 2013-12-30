bb26ab92768c03e945b9f7604cf0d795d835a5eb
Lee McDermott <github@leemcdermott.co.uk>
2013-12-30 04:52:51 +0000

    Fix

--- PaymentKit Example/PaymentKit Example/AppDelegate.m
+++ PaymentKit Example/PaymentKit Example/AppDelegate.m
@@ -8,8 +8,6 @@
 
 #import "AppDelegate.h"
 
-#import "ViewController.h"
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
