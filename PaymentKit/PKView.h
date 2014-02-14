829e5a6435d5785fbc12cb2af812d935f175d2a5
Phil Cohen <phil@stripe.com>
2014-02-13 18:41:18 -0800

    Fix: missing weak declaration (thanks , mrmonopoly237!)

--- PaymentKit/PKView.h
+++ PaymentKit/PKView.h
@@ -37,7 +37,7 @@
 @property IBOutlet PKTextField* cardExpiryField;
 @property IBOutlet PKTextField* cardCVCField;
 @property IBOutlet UIImageView* placeholderView;
-@property id <PKViewDelegate> delegate;
+@property (weak) id <PKViewDelegate> delegate;
 @property (readonly) PKCard* card;
 
 @end
