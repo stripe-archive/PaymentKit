324dd153d2ba0cd2bc280476ad9bfc144fd43900
Douglas Bumby <douglas.bumby@icloud.com>
2015-07-19 16:24:58 +0100

    Framework project

---
+++ PaymentKit/Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Douglas Bumby on 2015-07-19.
+//  Copyright (c) 2015 Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PaymentKit/PaymentKit/PaymentKit.h
@@ -0,0 +1,19 @@
+//
+//  PaymentKit.h
+//  PaymentKit
+//
+//  Created by Douglas Bumby on 2015-07-19.
+//  Copyright (c) 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PaymentKit.
+FOUNDATION_EXPORT double PaymentKitVersionNumber;
+
+//! Project version string for PaymentKit.
+FOUNDATION_EXPORT const unsigned char PaymentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymentKit/PublicHeader.h>
+
+
