5aea1a32132cc3c7305258dc0699b7a992f02f76
Douglas Bumby <douglas.bumby@icloud.com>
2015-07-19 16:31:15 +0100

    Add public headers

--- PaymentKit/PaymentKit.h
+++ PaymentKit/PaymentKit.h
@@ -16,4 +16,13 @@ FOUNDATION_EXPORT const unsigned char PaymentKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PaymentKit/PublicHeader.h>
 
-
+#import <PaymentKit/PTKTextField.h>
+#import <PaymentKit/PTKView.h>
+#import <PaymentKit/PTKAddressZip.h>
+#import <PaymentKit/PTKUSAddressZip.h>
+#import <PaymentKit/PTKCard.h>
+#import <PaymentKit/PTKCardCVC.h>
+#import <PaymentKit/PTKCardExpiry.h>
+#import <PaymentKit/PTKCardNumber.h>
+#import <PaymentKit/PTKCardType.h>
+#import <PaymentKit/PTKComponent.h>
